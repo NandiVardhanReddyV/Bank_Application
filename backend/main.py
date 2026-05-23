a0c77bf32de3afbc867e5d3c1da26c8e9603c9b2
NandiVardhanReddyV <vogulamnandivardhan@gmail.com>
2026-05-23 10:01:52 +0530

    Enable CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,10 @@ from auth import(
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://bank-application-gold.vercel.app",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
